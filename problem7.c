13365384f24c916ef16e857285c278fbaf9d1b5c
usman-murtza-47 <usmanmurtza11@gmail.com>
2025-08-21 23:26:27 +0530

    Update problem7.c

--- problem7.c
+++ problem7.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
-int reverseArray(int *arr,int n){
-    int *ptr=&arr[n-1];
-    printf("Accessing Reverse array elements using pointers:");
-    printf("\n");
-    for(int i=0;i<n;i++){
-        printf("%d ",*ptr);
-        ptr--;
-        
+int reverseArray(int *arr,int n){ 
+  int *str=arr;
+  int *end=&arr[n-1];
+    while(str<end){
+        int temp;
+        temp=*str;
+        *str=*end;
+        *end=temp;
+        str++;
+        end--;
+    }
+    return 0;
+}
+
+int printArray(int *arr,int n){
+    int *ptr=arr;
+for(int i=0;i<5;i++){
+    printf("%d ",*ptr);
+    ptr++;
     }
     return 0;
 }
 
 int main() {
 int arr[5]={1,2,3,4,5};
-int *ptr=arr;
-printf("Accessing array elements using pointers:");
+printf("Accessing array elements before reversing:");
+printArray(arr ,5);
 printf("\n");
-for(int i=0;i<5;i++){
-    printf("%d ",*ptr);
-    ptr++;
-    }
-    printf("\n");
+printf("Accessing array elements after reversing:");
 reverseArray(arr,5);
-    return 0;
+printArray(arr ,5);    
+return 0;
 }
